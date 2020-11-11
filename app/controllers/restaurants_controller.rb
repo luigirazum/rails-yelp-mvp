0b6cd24d2b425a4551ee7bdbd6bcfb7102201a50
Luis Zubia <soyzubia@gmail.com>
2020-11-11 14:47:03 -0700

    Some adjustments, simple_form in new restaurant.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,9 +14,12 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-
-    redirect_to restaurant_path(@restaurant)
+    
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
