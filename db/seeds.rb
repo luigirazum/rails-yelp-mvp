662ef92a0e8aa0daf37309fb135c1ea6a0b5d3fc
Luis Zubia <soyzubia@gmail.com>
2020-11-11 11:56:15 -0700

    Seeding Restaurants completed. OK.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Seeding restaurants. STARTED..."
+5.times do
+  restaurant = Restaurant.new
+  restaurant.name = Faker::Restaurant.name
+  restaurant.address = Faker::Address.street_address
+  restaurant.phone_number = Faker::PhoneNumber.cell_phone
+  restaurant.category = %w(chinese italian japanese french belgian).sample
+  
+  restaurant.save
+end
+
+puts "Seeding restaurants. FINISHED!"
